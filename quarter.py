9423f82de8f5ceff81b0143c536cf2a6af3bfcec
xzqt23 <angelo.agualada@gmail.com>
2023-03-15 10:13:38 +0800

    Create quarter.py

---
+++ quarter.py
@@ -0,0 +1,10 @@
+def quarter_of(month):
+    if month >= 1 and month <= 3: return 1
+    
+    elif month >= 4 and month <= 6: return 2
+
+    elif month >= 7 and month <= 9 : return 3
+    
+    else: return 4
+
+
